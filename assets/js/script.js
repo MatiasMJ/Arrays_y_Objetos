46bb32f61420f8a66a556c6c9afec99a2b934819
Matias Mesa <matias.mesaj@gmail.com>
2022-02-15 17:36:32 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,162 @@
+const radiologia = [
+  {
+    HORA: "11:00",
+    ESPECIALISTA: "IGNACIO SCHULZ",
+    PACIENTE: "FRANCISCA ROJAS",
+    RUT: "9878782-1",
+    PREVISION: "FONASA",
+  },
+  {
+    HORA: "11:30",
+    ESPECIALISTA: "FEDERICO SUBERCASEAUX",
+    PACIENTE: "PAMELA ESTRADA",
+    RUT: "15345241-3",
+    PREVISION: "ISAPRE",
+  },
+  {
+    HORA: "15:00",
+    ESPECIALISTA: "FERNANDO WURTHZ",
+    PACIENTE: "ARMANDO LUNA",
+    RUT: "16445345-9",
+    PREVISION: "ISAPRE",
+  },
+  {
+    HORA: "15:30",
+    ESPECIALISTA: "ANA MARIA GODOY",
+    PACIENTE: "MANUEL GODOY",
+    RUT: "17666419-0",
+    PREVISION: "FONASA",
+  },
+  {
+    HORA: "16:00",
+    ESPECIALISTA: "PATRICIA SUAZO",
+    PACIENTE: "RAMON ULLOA",
+    RUT: "14989389-K",
+    PREVISION: "FONASA",
+  },
+];
+
+const traumatologia = [
+  {
+    HORA: "8:00",
+    ESPECIALISTA: "MARIA PAZ ALTUZARRA",
+    PACIENTE: "PAULA SANCHEZ",
+    RUT: "15554774-5",
+    PREVISION: "FONASA",
+  },
+  {
+    HORA: "10:00",
+    ESPECIALISTA: "RAUL ARAYA",
+    PACIENTE: "ANGELICA NAVAS",
+    RUT: "15444147-9",
+    PREVISION: "ISAPRE",
+  },
+  {
+    HORA: "10:30",
+    ESPECIALISTA: "MARIA ARRIAGADA",
+    PACIENTE: "ANA KLAPP",
+    RUT: "17879423-9",
+    PREVISION: "ISAPRE",
+  },
+  {
+    HORA: "11:00",
+    ESPECIALISTA: "ALEJANDRO BADILLA",
+    PACIENTE: "FELIPE MARDONES",
+    RUT: "1547423-6",
+    PREVISION: "ISAPRE",
+  },
+  {
+    HORA: "11:30",
+    ESPECIALISTA: "CECILIA BUDNIK",
+    PACIENTE: "DIEGO MARRE",
+    RUT: "16554741-K",
+    PREVISION: "FONASA",
+  },
+  {
+    HORA: "12:00",
+    ESPECIALISTA: "ARTURO CAVAGNARO",
+    PACIENTE: "CECILIA MENDEZ",
+    RUT: "9747535-8",
+    PREVISION: "ISAPRE",
+  },
+  {
+    HORA: "12:30",
+    ESPECIALISTA: "ANDRES KANACRI",
+    PACIENTE: "MARCIAL SUAZO",
+    RUT: "11254785-5",
+    PREVISION: "ISAPRE",
+  },
+];
+
+const dental = [
+  {
+    HORA: "8:30",
+    ESPECIALISTA: "ANDREA ZU�IGA",
+    PACIENTE: "MARCELA RETAMAL",
+    RUT: "11123425-6",
+    PREVISION: "ISAPRE",
+  },
+  {
+    HORA: "11:00",
+    ESPECIALISTA: "MARIA PIA ZA�ARTU",
+    PACIENTE: "ANGEL MU�OZ",
+    RUT: "9878789-2",
+    PREVISION: "ISAPRE",
+  },
+  {
+    HORA: "11:30",
+    ESPECIALISTA: "SCARLETT WITTING",
+    PACIENTE: "MARIO KAST",
+    RUT: "7998789-5",
+    PREVISION: "FONASA",
+  },
+  {
+    HORA: "13:00",
+    ESPECIALISTA: "FRANCISCO VON TEUBER",
+    PACIENTE: "KARIN FERNANDEZ",
+    RUT: "18887662-K",
+    PREVISION: "FONASA",
+  },
+  {
+    HORA: "13:30",
+    ESPECIALISTA: "EDUARDO VI�UELA",
+    PACIENTE: "HUGO SANCHEZ",
+    RUT: "17665461-4",
+    PREVISION: "FONASA",
+  },
+  {
+    HORA: "14:00",
+    ESPECIALISTA: "RAQUEL VILLASECA",
+    PACIENTE: "ANA SEPULVEDA",
+    RUT: "14441281-0",
+    PREVISION: "ISAPRE",
+  },
+];
+
+const primeraAtencionRadiologia = document.getElementById(
+  "primeraAtencionRadiologia"
+);
+const primeraAtencionTraumatologia = document.getElementById(
+  "primeraAtencionTraumatologia"
+);
+const primeraAtencionDental = document.getElementById(
+  "primeraAtencionDental"
+);
+
+const ultimaAtencionRadiologia = document.getElementById(
+  "ultimaAtencionRadiologia"
+);
+const ultimaAtencionTraumatologia = document.getElementById(
+  "ultimaAtencionTraumatologia"
+);
+const ultimaAtencionDental = document.getElementById(
+  "ultimaAtencionDental"
+);
+
+primeraAtencionRadiologia.innerHTML = `${radiologia[0].PACIENTE} - ${radiologia[0].PREVISION}`
+primeraAtencionTraumatologia.innerHTML = `${radiologia[0].PACIENTE} - ${radiologia[0].PREVISION}`
+primeraAtencionDental.innerHTML = `${radiologia[0].PACIENTE} - ${radiologia[0].PREVISION}`
+
+ultimaAtencionRadiologia.innerHTML = `${radiologia[radiologia.length - 1].PACIENTE} - ${radiologia[radiologia.length - 1].PREVISION}`
+ultimaAtencionTraumatologia.innerHTML = `${traumatologia[traumatologia.length - 1].PACIENTE} - ${traumatologia[traumatologia.length - 1].PREVISION}`
+ultimaAtencionDental.innerHTML = `${dental[dental.length - 1].PACIENTE} - ${dental[dental.length - 1].PREVISION}`
